2cf6d36077407a611a36a951fe66a783e0fa102c
Partha <parthapratimpaul2003@gmail.com>
2024-08-18 21:38:01 +0530

    Added Custom MiddleWare to Show Server Requests

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,25 @@ const authRoutes = require('./authentication');
 const homeRoutes = require('./home');
 const port = process.env.PORT || 3000;
 
+function getFormattedDateTime() {
+    const now = new Date();
+    const date = now.toLocaleDateString('en-GB'); // Format: DD/MM/YYYY
+    const time = now.toLocaleTimeString('en-GB'); // Format: HH:MM:SS
+    return `${date} ${time}`;
+}
+
+// Custom middleware to log requests with formatted time, method, URL, and response code
+app.use((req, res, next) => {
+    const startTime = new Date();
+    let url = req.url;
+    res.on('finish', () => {
+        const elapsedTime = new Date() - startTime;
+        console.log(`[${getFormattedDateTime()}] ${req.method} ${url} - ${res.statusCode} (${elapsedTime}ms)`);
+    });
+
+    next();
+});
+
 //body parser middleware
 app.use(bodyParser.json());
 
