86b6200cc7d44fa7fd7d54279f269df6d5731d46
Partha <parthapratimpaul2003@gmail.com>
2024-08-18 21:44:40 +0530

    Fixed Bug with Showing Server Requests

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,25 @@ const port = process.env.PORT || 3000;
 //body parser middleware
 app.use(bodyParser.json());
 
+function getFormattedDateTime() {
+    const now = new Date();
+    const date = now.toLocaleDateString('en-GB'); // Format: DD/MM/YYYY
+    const time = now.toLocaleTimeString('en-GB'); // Format: HH:MM:SS
+    return `${date} ${time}`;
+}
+
+// Custom middleware to log requests with formatted time, method, URL, and response code
+app.use((req, res, next) => {
+    const startTime = new Date();
+    let url = req.url;
+    res.on('finish', () => {
+        const elapsedTime = new Date() - startTime;
+        console.log(`[${getFormattedDateTime()}] ${req.method} ${url} - ${res.statusCode} (${elapsedTime}ms)`);
+    });
+
+    next();
+});
+
 //routes middleware
 app.use("/auth", authRoutes);
 app.use("/home", homeRoutes);
