579eb3b7ae4fb7d92393c4b72a3f21342f3f0b29
Dedipya Goswami <111869712+dedipya001@users.noreply.github.com>
2024-09-14 00:39:28 +0530

    Applied SEO to the profile page

--- Frontend/src/components/Profile/Profile.jsx
+++ Frontend/src/components/Profile/Profile.jsx
@@ -13,6 +13,8 @@ import Sidebar from "../Left Sidebar/Sidebar";
 import UserQuestions from "./UserQuestions";
 import SavedQuestions from "./SavedQuestions";
 
+import { Helmet } from "react-helmet";
+
 const Profile = () => {
   const { isDarkMode } = useTheme();
   const [user, setUser] = useState(null);
@@ -20,6 +22,7 @@ const Profile = () => {
   const [refreshActivityTrigger, setRefreshActivityTrigger] = useState(0);
   const [activeSection, setActiveSection] = useState("profile");
   const [isMainLoading, setIsMainLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -32,6 +35,8 @@ const Profile = () => {
       );
       if (response.data.isAuthenticated) {
         setUser(response.data.user);
+        console.log(response.data.user);
+        
       }
     } catch (err) {
       console.error("Error fetching user data:", err);
@@ -86,8 +91,29 @@ const Profile = () => {
     );
   }
 
-  return (
+
+  const pageTitle = user ? `${user.first_name}'s Profile | The One Interview` : 'User Profile | The One Interview';
+  const pageDescription = user
+    ? `View ${user.name}'s profile on The One Interview. Username: ${user.username}. Explore their Posts, Questions, and more.`
+    : 'View user profile on The One Interview. Explore their Posts, Questions, and more.';   
+    
+    return (
     <div className={`profile-page ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+
+<Helmet>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={pageDescription} />
+        <meta property="og:type" content="profile" />
+        <meta property="og:url" content={`https://the-one-interview.vercel.app/${location.pathname}`} />  {/* Change this when the profile page for each user will be made */}
+        {user && user.profile_pic && <meta property="og:image" content={user.profile_pic} />}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={pageDescription} />
+        {user && user.profile_pic && <meta name="twitter:image" content={user.profile_pic} />}
+        <link rel="canonical" href={`https://the-one-interview.vercel.app/${location.pathname}`} />  {/* Change this when the profile page for each user will be made */}
+      </Helmet>
       <NavBar />
       <div className="profile-main-content">
         <Sidebar
